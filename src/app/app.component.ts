02df35fa1bcb7b6374b148715b99157f198aa679
lucas <lucasmanzanel@gmail.com>
2022-04-10 23:58:14 -0300

    form-manzanel-lucas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  formIncluit!: FormGroup;
+
+  constructor(private fb:FormBuilder){
+
+    this.formIncluit = this.fb.group({
+      nombre: ['', [Validators.required]],
+      edad: ['', [Validators.required, Validators.max(180)]],
+      empresa: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+    })
+
+  }
+
+
+  get email() { return this.formIncluit.get('email')}
+  get edad () {return this.formIncluit.get('edad')}
+  get empresa () {return this.formIncluit.get('empresa')}
+  get nombre () {return this.formIncluit.get('nombre')}
+
+
+
+  enviar(){
+    console.log(this.formIncluit.value)
+  }
   
 }
